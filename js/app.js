2e5134e4dab6fc46a2151b63c4c59547aeec6d91
Rakesh Patel <emailme@rakeshrpatel.com>
2016-05-04 22:36:10 +0530

    fiddling w/ angular

--- js/app.js
+++ js/app.js
@@ -1,28 +1,31 @@
-console.log("working")
-
-// 1. Upon click, need to validate that user has entered number
-// 		1A. If valid, customerCharges & Earnings info updates
-// 				1.1. Need to do calculations
-// 				1.2. Need to keep cumulative Tip total, meal count, and updated average in Earnings section
-// 		1B. If invalid, need to tell user what went wrong and allow for resubmission
-
-
-// Hit submit ---> values go to controller to make sure they are correct ---> No, then send error message
-// Hit submit (ng-submit=function()) --> Values go to controller for evaluation ---> calculations are made ---> return values to View
-// Tip amount gets passed to EarnInfo, meal count++, and two values are divided ---> value gets returned to view
-
 angular.module("waitstaffCalc", [])
 	.controller("updateContent", function($scope){
 
 		$scope.defaultScope = false;
+		// start mealcount. Starts at 1 to bring initial displayed value to 1.
 		var mealCount = 1;
-		$scope.tips = {};
+		// create a new object to store tip values throughout submissions.
 
+		$scope.tips = [];
+		
+		// For loop that iterates through the array values, adding them up.
+		$scope.tipSummation = function(){
+			for(var i=0; i<$scope.tips.length; i++){
+				// Need to create a new global variable variable.
+				return $scope.total = 0;
+				console.log($scope.total);
+			}
+		}
+
+
+		// triggered upon ng-click w/ button. Tried with form submission but didn't work properly(reason?)
 		$scope.submit = function(){
 			$scope.defaultScope = $scope.waitstaffInput.$valid;
+			//Calculates # of meals and pushes to view 
 			$scope.mealCount = mealCount++;
-			$scope.tips.push({{tipPercent}});
-			console.log($scope.tips)
+			// Push value from view to new object(or array?) 
+			$scope.tips.push($scope.tipPercent);
+			console.log($scope.tips);
 			
 		}; 
 
