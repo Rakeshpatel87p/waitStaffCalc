c259404f999e97599ee87e56b7361baa797f46b8
Rakesh Patel <emailme@rakeshrpatel.com>
2016-05-24 15:03:27 -0400

    still working on controller

--- js/app.js
+++ js/app.js
@@ -1,56 +1,92 @@
-angular.module("waitstaffCalc", [])
-	.controller("updateContent", function($scope){
+angular.module("waitstaffCalc", ['ngRoute', 'ngAnimate'])
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider
 
-		$scope.defaultScope = false;
-		// start mealcount. Starts at 1 to bring initial displayed value to 1.
-		var mealCount = 1;
-		// create a new object to store tip values throughout submissions.
-		$scope.tips = [];
+        .when('/home', {
+            templateUrl: 'home.html',
+            controller: 'updateContent',
+        })
+            .when('/new-meal', {
+            templateUrl: 'new-meal.html',
+            controller: 'updateContent',
+        })
 
-		// triggered upon ng-click w/ button. Tried with form submission but didn't work properly(reason?)
-		$scope.submit = function(){
-			// Makes sure numbers are valid numbers
-			$scope.defaultScope = $scope.waitstaffInput.$valid;
-			
-			// Conduct calculations for Customer Charges Field
-			$scope.subTotalAmount = $scope.baseMealPrice + ($scope.taxRate/100 * $scope.baseMealPrice);
-			$scope.tipPercentAmount = ($scope.tipPercent/100) * $scope.baseMealPrice;
-			$scope.totalAmount = $scope.subTotalAmount + $scope.tipPercentAmount;
-			
-			// Push value from view to array
-			$scope.tips.push($scope.tipPercentAmount);
-			console.log($scope.tips);
-			
-			//Calculates # of meals and pushes to view 
-			$scope.mealCount = mealCount++;
-			$scope.tipSummation();
+        .when('/my-earnings', {
+            templateUrl: 'my-earnings.html',
+            controller: 'updateContent'
+        })
 
-		};
-		// For loop that iterates through the array values, adding them up.
-		$scope.tipSummation = function(){
-			var total = 0;
-			for(var i=0; i<$scope.tips.length; i++){								
-				total += $scope.tips[i];
-			}
+        .otherwise({ redirectTo: '/home' })
+    }])
 
-			return total;
-		};
-		// Keeps an eye on the tips array and makes changes when it notices a change
-		$scope.$watchCollection("tips", function() {
-    		$scope.totalTips = $scope.tipSummation();
-  });
+.run(function($rootScope, $location, $timeout) {
+    $rootScope.$on('$routeChangeError', function() {
+        $location.path('/error');
+    });
 
-// Clear fields upon cancel button click
-	$scope.clearMealDetailFields = function(){
-		$scope.baseMealPrice = "";
-		$scope.taxRate = "";
-		$scope.tipPercent = "";
-	};
+    $rootScope.$on('$routeChangeStart', function() {
+        $rootScope.isLoading = true;
+    });
 
-	$scope.resetForm = function(){
-		$scope.clearMealDetailFields();
-		$scope.defaultScope = false;
-		mealCount=1;
-		$scope.tips = [];
-	}
+    $rootScope.$on('$routeChangeSuccess', function() {
+        $timeout(function() {
+            $rootScope.isLoading = false;
+        }, 500);
+    });
+})
+
+.controller("updateContent", function($rootScope, $scope) {
+
+    $rootScope.defaultScope = false;
+    // start mealcount. Starts at 1 to bring initial displayed value to 1.
+    $rootScope.mealCount = 0;
+    // create a new object to store tip values throughout submissions.
+    $scope.tips = [];
+
+    // triggered upon ng-click w/ button. Tried with form submission but didn't work properly(reason?)
+    $scope.submit = function() {
+        // Makes sure numbers are valid numbers
+        $rootScope.defaultScope = $scope.waitstaffInput.$valid;
+
+        // Conduct calculations for Customer Charges Field
+        $rootScope.subTotalAmount = $scope.baseMealPrice + ($scope.taxRate / 100 * $scope.baseMealPrice);
+        $rootScope.tipPercentAmount = ($scope.tipPercent / 100) * $scope.baseMealPrice;
+        $rootScope.totalAmount = $scope.subTotalAmount + $scope.tipPercentAmount;
+
+        // Push value from view to array
+        $scope.tips.push($rootScope.tipPercentAmount);
+
+        //Calculates # of meals and pushes to view 
+        $rootScope.mealCount++;
+        console.log($rootScope.mealCount);
+        $rootScope.tipSummation();
+
+    };
+    // For loop that iterates through the array values, adding them up.
+    $rootScope.tipSummation = function() {
+        var total = 0;
+        for (var i = 0; i < $scope.tips.length; i++) {
+            total += $scope.tips[i];
+        }
+        console.log()
+        return total;
+    };
+    // Keeps an eye on the tips array and makes changes when it notices a change
+    $rootScope.$watchCollection("tips", function() {
+        $rootScope.totalTips = $rootScope.tipSummation();
+    });
+
+    // Clear fields upon cancel button click
+    $rootScope.clearMealDetailFields = function() {
+        $rootScope.baseMealPrice = "";
+        $rootScope.taxRate = "";
+        $rootScope.tipPercent = "";
+    };
+
+    $rootScope.resetForm = function() {
+        $rootScope.clearMealDetailFields();
+        $rootScope.defaultScope = false;
+        $rootScope.mealCount = 0;
+        $rootScope.tips = [];
+    }
 });
